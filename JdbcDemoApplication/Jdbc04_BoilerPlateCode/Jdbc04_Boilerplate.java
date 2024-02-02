f3281094282e53f4e537589adf159033c483f50f
kunal kanse <92772714+kunalkanse@users.noreply.github.com>
2024-02-02 22:58:54 +0530

    Update Jdbc04_Boilerplate.java

--- JdbcDemoApplication/Jdbc04_BoilerPlateCode/Jdbc04_Boilerplate.java
+++ JdbcDemoApplication/Jdbc04_BoilerPlateCode/Jdbc04_Boilerplate.java
@@ -7,7 +7,7 @@ import java.sql.*;
 public class Jdbc04_Boilerplate {
     
     Statement statement = null;
-    // Connection conn = null;
+    // Connection conn = null; 
     
     public void setConnection(){
         try {
