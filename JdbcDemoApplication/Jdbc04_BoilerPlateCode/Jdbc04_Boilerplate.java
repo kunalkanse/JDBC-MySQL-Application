849d1818e92f880b3033c404bd9cf06b600968d6
kunal kanse <92772714+kunalkanse@users.noreply.github.com>
2024-02-02 22:58:42 +0530

    Update Jdbc04_Boilerplate.java

--- JdbcDemoApplication/Jdbc04_BoilerPlateCode/Jdbc04_Boilerplate.java
+++ JdbcDemoApplication/Jdbc04_BoilerPlateCode/Jdbc04_Boilerplate.java
@@ -5,11 +5,16 @@ package JdbcDemoApplication.Jdbc05_BoilerMain;
 import java.sql.*;
 
 public class Jdbc04_Boilerplate {
+    
     Statement statement = null;
-
+    // Connection conn = null;
+    
     public void setConnection(){
         try {
             statement = DriverManager.getConnection("jdbc:mysql://localhost:3306/empj2ee","root","Kunal@447").createStatement();
+            
+            // Most of the cases we have to return Object of Connection Only because based on Requirment we may need Statement, PreparedStatement or callableStatement
+            // conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/empj2ee","root","Kunal@447")
         } catch (SQLException e) {
             System.out.println(e);
         }
@@ -17,5 +22,6 @@ public class Jdbc04_Boilerplate {
 
     public Statement getStatement() {
         return statement;
+        // return conn;
     }
 }
